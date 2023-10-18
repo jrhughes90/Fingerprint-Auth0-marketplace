403a53bff400971751643af68be2d2a63a8a5627
Juraj Uhlar <juro.uhlar@gmail.com>
2023-10-18 09:30:28 +0100

    Update fingerprint-fp-auth0-pre-user-registration-action.js

--- pre-user-registration-action/fingerprint-fp-auth0-pre-user-registration-action.js
+++ pre-user-registration-action/fingerprint-fp-auth0-pre-user-registration-action.js
@@ -1,73 +1,78 @@
 /**
-* Handler that will be called during the execution of a PreUserRegistration flow.
-*
-* @param {Event} event - Details about the context and user that is attempting to register.
-* @param {PreUserRegistrationAPI} api - Interface whose methods can be used to change the behavior of the signup.
-*/
+ * Handler that will be called during the execution of a PreUserRegistration flow.
+ *
+ * @param {Event} event - Details about the context and user that is attempting to register.
+ * @param {PreUserRegistrationAPI} api - Interface whose methods can be used to change the behavior of the sign-up.
+ */
 exports.onExecutePreUserRegistration = async (event, api) => {
-    //get fingerprint value passed in from sign-up request
-    //in this example a value of signup_fingerprint is sent as part of the app_metadata
-    const fingerprint = event.user.app_metadata?.signup_fingerprint;
-    if (!fingerprint) {
-        return;
+  // Get Fingerprint data from app_metadata
+  const visitorId = event.user.app_metadata?.visitorId;
+  const requestId = event.user.app_metadata?.requestId;
+  if (!requestId || !visitorId) {
+    console.log("No fingerprint data provided.");
+    return;
+  }
+
+  try {
+    // Using the request ID, get the complete identification event from the Fingerprint Server API
+    const { FingerprintJsServerApiClient, Region } = require("@fingerprintjs/fingerprintjs-pro-server-api");
+    // Use your Fingerprint application's region and Secret API Key
+    const client = new FingerprintJsServerApiClient({
+      region: Region.EU,
+      apiKey: event.secrets.api_key,
+    });
+    const fingerprintEvent = await client.getEvent(requestId);
+    console.log(fingerprintEvent);
+
+    // Verify that the provided visitor ID matches the one from Fingerprint servers
+    var serverApiVisitorId = fingerprintEvent.products.identification.data.visitorId;
+    console.log("Visitor ID from Fingerprint Server API: " + serverApiVisitorId);
+    console.log("Received visitor ID: " + visitorId);
+    if (serverApiVisitorId !== visitorId) {
+      api.access.deny("tampering_detected", "Sign-ups from this device cannot be accepted.");
     }
-    var array = fingerprint.split(" ");
-    const visitorId = array[0]
-    const requestId = array[1]
-    console.log("VisitorId is:" + visitorId);
-    console.log("RequestId is:" + requestId);
 
-    //spin up fingerpint client 
-    const { FingerprintJsServerApiClient, Region } = require('@fingerprintjs/fingerprintjs-pro-server-api');
-    // Init client with the given region and the secret api_key
-    const client = new FingerprintJsServerApiClient({ region: Region.EU, apiKey: event.secrets.api_key });
-    client.getEvent(requestId).then((event) => {
-        var visIdCheck = event.products.identification.data.visitorId;
-        console.log("Visitor Id tampering check: " + visIdCheck + " Visitor Id sent in request: " + visitorId)
-        if (visIdCheck !== visitorId) {
-            api.access.deny("tampering_detected", 'Sign-ups from this device cannot be accepted');
-        }
-        //optional - check smart signals here
-        console.log("Bot Detected : " + event.products.botd.data.bot.result);
-        if (event.products.botd.data.bot.result === "bad") {
-            api.access.deny('bot_detected', 'Bot detected');
-        }
-        // console.log("Known to IP Blocklists : " + event.products.ipBlocklist.data.result);
-        // console.log("Virtual Machine Detected : " + event.products.virtualMachine.data.result);
-        // console.log("Tor Network Detected : " + event.products.tor.data.result);
+    // Optional - examine Fingerprint Smart Signals for additional checks
+    if (fingerprintEvent.products.botd.data.bot.result === "bad") {
+      api.access.deny("bot_detected", "Bot detected");
+    }
+    // console.log("Known to IP Blocklists : " + event.products.ipBlocklist.data.result);
+    // console.log("Virtual Machine Detected : " + event.products.virtualMachine.data.result);
+    // console.log("Tor Network Detected : " + event.products.tor.data.result);
+  } catch (err) {
+    // In case of error, fail silently - don't block sign-up
+    console.log("Fingerprint-related error: ", err);
+  }
 
-    }).catch(function (err) {
-        console.log(err)
+  try {
+    // Search for other users with the same visitor ID using the Auth0 Management API
+    // This example uses app_metadata to associate users with visitor IDs,
+    // but we recommend using your own user storage in production
+    const ManagementClient = require("auth0").ManagementClient;
+    const auth0ManagementClient = new ManagementClient({
+      domain: event.secrets.domain,
+      clientId: event.secrets.clientId,
+      clientSecret: event.secrets.clientSecret,
     });
-    //query existing users for same fingerprint
-    //this could be using Auth0 app_metadata (not recommended for production) as shown below 
-    //or using your own user store / API backend (recommended)
 
-    //load Auth0 management client
-    const ManagementClient = require('auth0').ManagementClient;
-    const management = new ManagementClient({
-        domain: event.secrets.domain,
-        clientId: event.secrets.clientId,
-        clientSecret: event.secrets.clientSecret,
-    });
-    var params = {
-        search_engine: 'v3',
-        q: 'app_metadata.visitorIds: ' + visitorId + '',
-        per_page: 10,
-        page: 0
+    const userSearchParams = {
+      search_engine: "v3",
+      q: "app_metadata.visitorIds: " + visitorId + "",
+      per_page: 10,
+      page: 0,
     };
-    try {
-        const res = await management.getUsers(params)
-        console.log("Count of users with same FP: " + res.length);
-        // determine number of allowed accounts per device to allow
-        if (res.length > 0) {
-            api.access.deny("max_device_limit", 'Further Sign-ups from this device cannot be accepted');
-        } else {
-            api.user.setAppMetadata("visitorIds", [visitorId]);
-        }
-    } catch (e) {
-        console.log(e)
-        // Handle error - fail silently - don't block signup
-    }
+    const visitorsWithTheSameVisitorId = (await auth0ManagementClient.users.getAll(userSearchParams)).data;
+    console.log("Number of users with same Fingerprint visitor ID: " + visitorsWithTheSameVisitorId.length);
 
+    // If there are too many users associated with this browser or device, deny the sign-up request
+    if (visitorsWithTheSameVisitorId.length > 0) {
+      api.access.deny("max_device_limit", "Too many sign-ups from this device.");
+    } else {
+      // If not, create the user and initialise their visitorIds array with the provided visitor ID
+      api.user.setAppMetadata("visitorIds", [visitorId]);
+    }
+  } catch (err) {
+    // In case of error, fail silently - don't block sign-up
+    console.log("Auth0-related error: ", err);
+  }
 };
