c1d3d955136537c314bd2f95be9bc1cfe0af470c
Jamie Hughes <jamiehughes@Jamies-MacBook-Air.local>
2024-10-29 16:25:54 +0000

    update to login-action

--- login-action/fp-auth0-login-action.js
+++ login-action/fp-auth0-login-action.js
@@ -1,50 +1,57 @@
 /**
-* Handler that will be called during the execution of a PostLogin flow.
-*
-* @param {Event} event - Details about the user and the context in which they are logging in.
-* @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
-*/
+ * Handler that will be called during the execution of a PostLogin flow.
+ *
+ * @param {Event} event - Details about the user and the context in which they are logging in.
+ * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
+ */
 exports.onExecutePostLogin = async (event, api) => {
-    // Pull visitorId, requestId from the query
-    const { visitorId, requestId } = event.request.query;
+    const {
+        FingerprintJsServerApiClient,
+        Region,
+    } = require("@fingerprintjs/fingerprintjs-pro-server-api");
 
-    if (!visitorId || !requestId) {
-        return;
-    }
-    console.log(`Fingerprint Pro values, visitorId: ${visitorId}, requestId: ${requestId}`);
-
-    // Use Fingerprint Pro Server API SDK to securely 
-    // retrieve complete info about that specific identification event
-    const { FingerprintJsServerApiClient, Region } = require('@fingerprintjs/fingerprintjs-pro-server-api');
     const client = new FingerprintJsServerApiClient({
         region: Region.Global,
-        apiKey: "YOUR_FINGERPRINT_SECRET_API_KEY" //event.secrets.api_key
+        apiKey: event.secrets.FINGERPRINT_SECRET_API_KEY,
     });
+
+    // Extract the visitorId and requestId from the query parameters
+    const { visitorId, requestId } = event.request.query;
+    if (!visitorId || !requestId)
+        return api.access.deny("Visitor identification not found.");
+
+    // Fetch the identification event using the requestId
     const identificationEvent = await client.getEvent(requestId);
-    console.log("event", JSON.stringify(identificationEvent, null, 2));
+    const eventVisitorId =
+        identificationEvent?.products?.identification?.data?.visitorId;
 
+    // Confirm the visitorId is the same as the one in the identification event
+    if (!eventVisitorId || visitorId !== eventVisitorId)
+        return api.access.deny("Visitor identification error.");
 
-    // Verify that the provided visitor ID matches the one from Fingerprint servers
-    var serverApiVisitorId = identificationEvent.products.identification.data.visitorId;
-    console.log("Visitor ID from Fingerprint Server API: " + serverApiVisitorId);
-    console.log("Received visitor ID: " + visitorId);
-    if (serverApiVisitorId !== visitorId) {
-        api.access.deny("tampering_detected", 'Sign-ups from this device cannot be accepted.');
-    }
+    // Save the visitorId within the app metadata
+    api.user.setAppMetadata("currentVisitorId", eventVisitorId);
 
-    const metadata = event.user.app_metadata;
+    // Get bot detection result
+    const botDetection = identificationEvent.products?.botd?.data?.bot?.result;
 
-    // Optional: Force multi-factor authentication for new visitor IDs
-    if (!metadata.visitorIds || !metadata.visitorIds.includes(visitorId)) {
-        api.multifactor.enable('any');
-    }
+    // If a bot is detected, deny the login
+    if (botDetection !== "notDetected") return api.access.deny("Login denied.");
 
-    // You can store the visitorId or use it in your authorization logic
-    // For example, you can add visitorId to user app metadata 
-    // to keep track of all of the user's browsers and devices  
-    if (!metadata.visitorIds) {
-        api.user.setAppMetadata("visitorIds", [visitorId]);
-    } else if (!metadata.visitorIds.includes(visitorId)) {
-        api.user.setAppMetadata("visitorIds", [visitorId, ...metadata.visitorIds]);
+    // If first login, enroll in MFA
+    const enrolledMFAs = event?.user?.multifactor?.length;
+    if (enrolledMFAs == 0 || event.stats.logins_count == 1)
+        return api.authentication.enrollWith({ type: "otp" });
+
+    // Otherwise, check if the visitorId is already associated with the user
+    // If not, prompt for MFA
+    const appMetadata = event.user.app_metadata || {};
+    if (!appMetadata.visitorIds || !appMetadata.visitorIds.includes(visitorId)) {
+        // Store the need for MFA verification in the app metadata
+        // This will be used in the following action
+        api.user.setAppMetadata("mfaNeeded", true);
+
+        // Trigger a MFA challenge, here we are using OTP
+        api.authentication.challengeWith({ type: "otp" });
     }
 };
